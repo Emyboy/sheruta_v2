15f132063f51be0b4add62b29570fbcbeaeb1fa6
Emyboy <emyboybeats@gmail.com>
2020-01-22 22:25:04 +0100

    fixed api error

--- server/App.js
+++ server/App.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, "/", "../build/")));
 
 app.get('*', (req, res) => {
-    res.sendfile(__dirname, '/', '../build');
+    res.sendfile(path.join(__dirname, "/", "../build/index.html"));
 });
 
 app.listen(port, () => {
